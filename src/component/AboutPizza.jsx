d76883b4eb0826ad79e9c99be8a8cfceb5018ac6
Eric Okyere <102455856+Eric-Okyere@users.noreply.github.com>
2022-07-02 15:44:55 +0000

    first commit

---
+++ src/component/AboutPizza.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+function AboutPizza({title}) {
+  return (
+    <>
+    <h1 class="text-center">About {title}</h1>
+    <div class="container">
+<div class="col-md-6">
+    <img src="Pic/13.jpg" alt=""/>
+</div>
+<div class="col-md-6">
+    <h3>Since its humble beginnings in Italy, pizza has gone through numerous transformations to become the dish that so many people know and love today. But if you’re just opening your first pizza place or want to add a pizza oven in your restaurant, it can be difficult to decide what style of pizza your restaurant will make. We discuss the different types of pizza, their history, and what you’ll need to make them, so you can start serving up delicious slices to your guests.</h3>
+</div>
+
+
+</div>
+    </>
+  )
+}
+
+export default AboutPizza
---
+++ src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div class="footer">
+    <div class="row">
+        <h1 class="text-center">&copy;2022 PIZZAMANIA All Right Researved</h1>
+    </div>
+</div>
+    </>
+  )
+}
+
+export default Footer
